c7f570d49aee8471a35880c602befe7af04a8ee6
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-05-30 11:48:01 +0530

    Mongodb aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,13 +233,16 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     await user.save({ validateBeforeSave: false })
     return res
         .status(200)
-        .json(new ApiError(200, {}, "Password Change Successfully"))
+        .json(new ApiResponse(200, {}, "Password Changed Successfully"))
 })
 
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "current user fetched Succcessfully")
+        .json(new ApiResponse(
+            200,
+            req.user,
+            "current user fetched Succcessfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -331,6 +334,84 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                  $cond:{
+                    if:  {$in: [req.user?._id, "$subscribers.subscriber"]} ,
+                     then: true,
+                      else: false
+
+                   }
+                }
+            } 
+        },
+        {
+            $project: {
+           fullname:1,
+           username:1,
+           subscribersCount:1,
+           channelsSubscribedToCount:1,
+           isSubscribed:1,
+           avatar:1,
+           coverImage:1,
+           email:1
+             
+
+
+            }
+        }
+
+
+    ])
+
+    if(!channel?.length){
+  throw new ApiError(404, "channel does not exist")
+    }
+return res
+     .status(200)
+     .json(
+        new ApiResponse(200, channel[0], "User channel fetched succcessfully")
+     )
+
+
+})
+
 export {
     updateUserCoverImage,
     updateUserAvatar,
@@ -340,5 +421,6 @@ export {
     loginUser,
     logoutUser,
     refreshAccessToken,
-    changeCurrentPassword
+    changeCurrentPassword,
+    getUserChannelProfile
 };
--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
         });
         console.log(`\n MONGODB connected !! DB HOST ${connectionInstance.connection.host}`);
     } catch (error) {
